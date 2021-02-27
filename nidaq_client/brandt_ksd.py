06801aafdd0f14537358365ad3cb041e8bbad1a4
waltoncade <waltoncade@gmail.com>
2021-02-27 15:49:29 -0800

    edited thermocouple module to add different couple types

--- nidaq_client/brandt_ksd.py
+++ nidaq_client/brandt_ksd.py
@@ -3,8 +3,8 @@ import csv
 from datetime import datetime,date
 import os
 
-task1 = nidaqmx.system.storage.persisted_task.PersistedTask('temp').load()
-# task1.timing.adc_sample_high_speed()
+task1 = nidaqmx.system.storage.persisted_task.PersistedTask('brandttemptask').load()
+task1.timing.adc_sample_high_speed()
 today = datetime.now()
 date = today.strftime("%Y-%m-%d")
 nidaqmx._task_modules.ai_channel_collection.AIChannelCollection
@@ -16,7 +16,7 @@ while os.path.exists('nidaq_client/Logs/{}_thrmcpllog_{}.csv'.format(date,iterat
 with open('nidaq_client/Logs/{}_thrmcpllog_{}.csv'.format(date,iteration),'w',newline='') as write_file:
     while True:
         csv_writer = csv.writer(write_file, delimiter= ',')
-        csv_writer.writerow([datetime.now().strftime("%H:%M:%S.%f"),"%.03f" % task1.read()])
+        csv_writer.writerow([datetime.now().strftime("%H:%M:%S.%f")[:-3],"%.03f" % task1.read()])
 
 write_file.close()
 task1.close()
